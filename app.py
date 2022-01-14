b0abcee1b6e13ea5f801437cd6c062e89ac6bd5e
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2022-01-14 21:22:26 +0530

    Please enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 # This is basically the heart of my flask 
 
 
@@ -10,43 +9,28 @@ from model import Recommendation
 recommend = Recommendation()
 app = Flask(__name__)  # intitialize the flaks app  # common 
 
-import os
-from flask import send_from_directory
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 @app.route('/', methods = ['POST', 'GET'])
 def home():
     flag = False 
     data = ""
+    
     if request.method == 'POST':
-        flag = True
-        user = request.form["userid"]
-        data=recommend.getTopProducts(user)
+        try:
+            flag = True
+            user = request.form["userid"]
+            data=recommend.getTopProducts(user)
+        except:
+            data="<center><b>UserNotFound</b></center>";
     return render_template('index.html', data=data, flag=flag)
 
-@app.route('/userList', methods = ['GET'])
-def userList():
-    data=recommend.getUsers()
-    return data
-
-@app.route('/productList', methods = ['GET'])
-def productList():
-    user=request.args.get("userid")
-    data=recommend.getTopProductsNew(user)
-    return data
-
-@app.route('/analysText', methods = ['GET'])
-def analysText():
-    text=request.args.get("text")
-    data=recommend.analyiseSentiment(text)
-    return data
 
 if __name__ == '__main__' :
-    app.run(debug=True )  
+    app.run(debug=True )  # this command will enable the run of your flask app or api
     
     #,host="0.0.0.0")
+
+
+
+
+
+
